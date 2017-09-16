2e4d64023019b9b0d9fd2a508777f0d2445f6aeb
Javier Diaz <coderdiaz@gmail.com>
2017-09-15 22:46:52 -0500

    Added browser-sync configuration

---
+++ browser-sync.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "server": "build",
+  "port": "8080",
+  "files": "build",
+  "open": false,
+  "notify": false,
+  "serveStatic": [
+    '_site'
+  ],
+  "serveStaticOptions": {
+    "extensions": ['html']
+  }
+}
